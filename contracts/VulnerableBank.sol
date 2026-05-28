d3267e626c6e9ed08a3440ea056a4bf5bce75c9d
Rimuru236 <levelsfashion39@gmail.com>
2026-05-28 10:16:05 -0700

    Added Solidity Vulnerable bank and securebank contracts

---
+++ contracts/VulnerableBank.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SecureBank {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint256 amount = balances[msg.sender];
+        require(amount > 0, "No balance");
+
+        balances[msg.sender] = 0; //<---------fixed line: state upated before external call
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Transfer failed");
+    }
+}
