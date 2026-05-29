c80470389d3e60a2d441bec7b2acf4debf0e9e51
Rimuru236 <levelsfashion39@gmail.com>
2026-05-28 18:59:23 -0700

    session 4: modified deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,15 @@ async function main() {
     const LabToken = await ethers.getContractFactory("LabToken");
     const labToken = await LabToken.deploy();
     await labToken.waitForDeployment();
-    console.log(
-        "Contract deployed to:",
-        await labToken.getAddress()
-    );
+
+    const address = await labToken.getAddress();
+    console.log("Contract deployed to:", address);
+
+    // Transfer tokens to your MetaMask wallet
+    const myMetaMaskAddress = ethers.getAddress("0x39952155336a0A8CE2E19fC163611bc8C6fCF97B");
+    const tx = await labToken.transfer(myMetaMaskAddress, ethers.parseEther("10000"));
+    await tx.wait();
+    console.log("Sent 10,000 LTK to", myMetaMaskAddress);
 }
 
 main().catch((err) => {
